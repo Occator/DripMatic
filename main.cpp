e10507cb4b6de8246c43caf4bf5b181e08a2d4e4
S.T.Ban <stefan@25livingroom.de>
2017-11-21 16:14:47 +0100

    added example for using the methods

--- main.cpp
+++ main.cpp
@@ -21,7 +21,29 @@ int main(){
 	_delay_ms(2000);
 	lcdTWI.set_Cursor(0, 2);
 	lcdTWI.write_String("ich bin es");
+	_delay_ms(2000);
+	lcdTWI.clear();
+	lcdTWI.write_String_XY(0, 1, "write StringXY()");
+	lcdTWI.write_String_XY(0, 2, "work too");
+	lcdTWI.no_Cursor();
+	_delay_ms(3000);
+	lcdTWI.cursor();
+	lcdTWI.no_Display();
+	_delay_ms(2000);
+	lcdTWI.clear();
+	lcdTWI.write_String_XY(0, 1, "Display was off");
+	lcdTWI.display();
+	_delay_ms(2000);
+	lcdTWI.write_String_XY(0, 2, "blink off");
+	lcdTWI.no_Blink();
+	_delay_ms(2000);
+	lcdTWI.home();
+	lcdTWI.write_String_XY(0, 2, "blink on ");
+	lcdTWI.blink();
+	_delay_ms(2000);
+	lcdTWI.clear();
+	lcdTWI.write_Int_XY(6, 2, 1300);
+
 
 	for(;;);
 }
-
