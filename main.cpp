b8f4136b4864fe7d32e78f719ec7a205bd1f4ccd
STBan <stban@posteo.de>
2017-11-28 20:29:53 +0100

    added i/o-pin class

--- main.cpp
+++ main.cpp
@@ -8,12 +8,16 @@
 #include "TWI/class_TWI.hpp"
 #include "TWI/class_lcd_1602.hpp"
 #include "ADC/class_adc_pin.hpp"
+#include "ADC/class_pin_io.hpp"
 
 
 int main(){
 
 	cTWI twiIOexpander(0x4E);
 	cLCD1602 lcdTWI(&twiIOexpander);
+
+	cIOPin redLED(&PORTB, 5, cIOPin::output);
+	redLED.set_Pin(1);
 
 	cADCPin LDR(0);
 	LDR.init();
@@ -27,6 +31,7 @@ int main(){
 	lcdTWI.no_Blink();
 	lcdTWI.no_Cursor();
 
+
 	for(;;)
 	{
 		lcdTWI.clear();
@@ -35,5 +40,6 @@ int main(){
 		lcdTWI.set_Cursor(7, 2);
 		lcdTWI.write_Int(adcValue);
 		_delay_ms(500);
+		redLED.set_Pin(0);
 	}
 }
