9d62fc69f24bff5ef423718c0b0e6914ca881bce
STBan <stban@posteo.de>
2017-12-05 21:26:07 +0100

    cleaned up src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -33,9 +33,6 @@ int main(){
 
 	cADCPin LDR(0);
 	LDR.init();
-	// temp sensor
-	cADCPin LM35(2);
-	LM35.init();
 
 	lcdTWI.init();
 
@@ -43,58 +40,16 @@ int main(){
 	lcdTWI.write_String("testing ADC");
 	_delay_ms(3000);
 
-
-	lcdTWI.set_Cursor(0, 1);
-	lcdTWI.clear();
-	lcdTWI.write_String("hey pat");
-	_delay_ms(2000);
-	lcdTWI.set_Cursor(0, 2);
-	lcdTWI.write_String("ich bin es");
-	_delay_ms(2000);
-	lcdTWI.clear();
-	lcdTWI.write_String_XY(0, 1, "write StringXY()");
-	lcdTWI.write_String_XY(0, 2, "work too");
-	lcdTWI.no_Cursor();
-	_delay_ms(3000);
-	lcdTWI.cursor();
-	lcdTWI.no_Display();
-	_delay_ms(2000);
-	lcdTWI.clear();
-	lcdTWI.write_String_XY(0, 1, "Display was off");
-	lcdTWI.display();
-	_delay_ms(2000);
-	lcdTWI.write_String_XY(0, 2, "blink off");
-	lcdTWI.no_Blink();
-	_delay_ms(2000);
-	lcdTWI.home();
-	lcdTWI.write_String_XY(0, 2, "blink on ");
-	lcdTWI.blink();
-	_delay_ms(2000);
-	lcdTWI.clear();
-	lcdTWI.write_Int_XY(6, 2, 1300);
-
-	int8_t count = 0;
-
 	for(;;)
 	{
 
-		if(count > 4)	yellowLED.set_Pin(0);
-		else yellowLED.set_Pin(1);
 		lcdTWI.clear();
 		lcdTWI.write_String_XY(4, 1, "ADC-Value");
 		auto adcValue = LDR.read();
 		lcdTWI.set_Cursor(7, 2);
 		lcdTWI.write_Int(adcValue);
 		_delay_ms(500);
-		lcdTWI.write_String_XY(2, 1, "Temperature");
-		auto tempValue = LM35.read();
-		lcdTWI.set_Cursor(7, 2);
-		lcdTWI.write_Int(tempValue);
-		_delay_ms(500);
-		redLED.set_Pin(0);
-		yellowLED.set_Pin(0);
-		_delay_ms(500);
-		count++;
+
 		// display date
 		ds1302.update_rtcTime();
 		lcdTWI.clear();
