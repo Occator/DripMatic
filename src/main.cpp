96766e577d596e367fbe4c8951303f506464acfe
STBan <stban@posteo.de>
2018-03-05 16:05:34 +0100

    added loop sending one character at time, starting with A and count up til Z

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,11 @@ int main(){
 
 	for(;;)
 	{
-		microSDCard.master_transmit(data);
-		data++;
-		_delay_ms(2000);
+		for(uint8_t i = 0; i < 26; i++)
+		{
+			microSDCard.master_transmit(data);
+			data++;
+			_delay_ms(2000);
+		}
 	}
 }
