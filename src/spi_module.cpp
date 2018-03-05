693981e41cd73d6c74b08cce3934e9f76612e80c
STBan <stban@posteo.de>
2018-03-05 21:44:27 +0100

    added clock-polarity and clock-phase to spi master_init()

--- src/spi_module.cpp
+++ src/spi_module.cpp
@@ -10,7 +10,7 @@ cSPIModule::~cSPIModule(){}
 void cSPIModule::master_init()
 {
   DDRB = (1 << PB3) | (1 << PB5) | (1 << PB2);
-  SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+  SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);  // | (1 << CPOL) | (1 << CPHA)
 }
 
 void cSPIModule::send_byte(uint8_t data)
