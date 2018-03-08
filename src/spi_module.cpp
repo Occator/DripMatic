8114880c7c75802075b8c621a99902d27c34d828
STBan <stban@posteo.de>
2018-03-08 16:10:24 +0100

    fixed setting data direction in master_init()

--- src/spi_module.cpp
+++ src/spi_module.cpp
@@ -9,8 +9,8 @@ cSPIModule::~cSPIModule(){}
 
 void cSPIModule::master_init()
 {
-  DDRB = (1 << PB3) | (1 << PB5) | (1 << PB2);
-  DDRB &= ~(1 << PB4);
+  DDRB = (1 << DDB3) | (1 << DDB5) | (1 << DDB2);
+  DDRB &= ~(1 << DDB4);
   PORTB |= (1 << PB2);
   SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);  // | (1 << CPOL) | (1 << CPHA)
 }
