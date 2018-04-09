6a676c4fc4b22166ae3b45f529d711dd400fc1c8
STBan <stban@posteo.de>
2018-04-09 18:14:20 +0200

    added sd Card commands to header-file

--- include/sdcard_module.h
+++ include/sdcard_module.h
@@ -4,8 +4,21 @@
 #include "spi_module.h"
 #include "pin_io.h"
 
-// formular for CMDs - command number n + 64 = 64 = 0x40
-#define CMD0 0x40
+// formular for CMDs - command number n | 0x40
+#define GO_IDLE_STATE 0
+#define SEND_OP_COND 1
+#define SEND_IF_COND 8
+#define SEND_CSD 9
+#define STOP_TRANSMISSION 12
+#define SEND_STATUS 13
+#define SET_BLOCK_LEN 16
+#define READ_SINGLE_BLOCK 17
+#define WRITE_SINGLE_BLOCK 24
+#define SD_SEND_OP_COND 41 // ACMD
+#define APP_CMD	55
+#define READ_OCR 58
+#define CRC_ON_OFF 59
+
 
 class cMicroSDModule
 {
