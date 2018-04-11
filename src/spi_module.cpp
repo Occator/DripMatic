a8ec825207f07583cd9d7781de007d0eb7911869
STBan <stban@posteo.de>
2018-04-11 23:45:25 +0200

    set SS (PB2) to default output

--- src/spi_module.cpp
+++ src/spi_module.cpp
@@ -35,6 +35,5 @@ void cSPIModule::init_Master()
 {
   DDRB = (1 << DDB3) | (1 << DDB5) | (1 << DDB2);
   DDRB &= ~(1 << DDB4);
-  PORTB |= (1 << PB2);
   SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1) | (1 << SPR0);
 }
