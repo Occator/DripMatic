4d92b71c6857657a863cf0be20c2c1376a00ddaa
STBan <stban@posteo.de>
2018-07-16 20:16:36 +0200

    deleted diskio.c and modified main.cpp

---
+++ src/diskio.cpp
@@ -0,0 +1,71 @@
+#include "diskio.h"
+#include "sdcard_module.h"
+#include "uart_module.h"
+#include "pin_io.h"
+#include "spi_module.h"
+
+
+static cMicroSDModule* sdCard_ = 0;
+
+static cUART uart;
+static cSPIModule spi;
+static cIOPin chipSelect(&PORTB, 2, cIOPin::output);
+static cMicroSDModule sdCard(&uart, &chipSelect, &spi);
+
+extern "C"
+{
+DSTATUS disk_status (
+	BYTE pdrv		/* Physical drive nmuber to identify the drive */
+)
+{
+	if( sdCard_ != 0 )
+		return 0;
+	return STA_NOINIT;
+}
+
+DSTATUS disk_initialize (
+	BYTE pdrv				/* Physical drive nmuber to identify the drive */
+)
+{
+  sdCard_ = &sdCard;
+
+  return RES_OK;
+}
+
+DRESULT disk_read (
+	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
+	BYTE *buff,		/* Data buffer to store read data */
+	DWORD sector,	/* Start sector in LBA */
+	UINT count		/* Number of sectors to read */
+)
+{
+  if( sdCard_ == 0 )
+    return RES_PARERR;
+  for(uint16_t i = 0; i < count; ++i)
+      sdCard_->readSingleBlock(buff+i*BLOCK_LENGTH, sector);
+  return RES_OK;
+}
+
+DRESULT disk_write (
+	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
+	const BYTE *buff,	/* Data to be written */
+	DWORD sector,		/* Start sector in LBA */
+	UINT count			/* Number of sectors to write */
+)
+{
+  if( sdCard_ == 0 )
+    return RES_PARERR;
+  for( uint16_t i = 0; i < count; ++i)
+    sdCard_->writeSingeBlock((uint8_t*)buff+i*BLOCK_LENGTH, sector);
+  return RES_OK;
+}
+
+DRESULT disk_ioctl (
+	BYTE pdrv,		/* Physical drive nmuber (0..) */
+	BYTE cmd,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
+{
+	return RES_PARERR;
+}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,41 +7,19 @@
 #include "spi_module.h"
 #include "sdcard_module.h"
 #include "uart_module.h"
+#include "diskio.h"
+#include "ff.h"
 
 
 int main(){
-	cUART sdComm;
-	sdComm.write_String("testing sd card init, read/write single block:\r\n");
 
-	_delay_ms(5000);
-	cIOPin spiCS(&PORTB, 2, cIOPin::output);
-	cSPIModule sdSPI;
-	cMicroSDModule microSD(&sdComm, &spiCS, &sdSPI);
+	FATFS fs;
+	FIL fil;
 
+	f_mount(&fs, "", 0);
 
-	uint8_t rwBuffer[512];
-	for(uint16_t j = 0; j < 512; j++)
-	{
-		rwBuffer[j] = 0;
-	}
-
-	for(uint8_t i = 0; i < 8; i++)
-	{
-		rwBuffer[i] = 13;
-	}
-
-	microSD.writeSingeBlock(rwBuffer, 5);
-
-	_delay_ms(2000);
-
-	sdComm.write_String("r/w buffer content:\r\n");
-	microSD.readSingleBlock(rwBuffer, 5);
-
-	for(uint16_t j = 0; j < 512; j++)
-	{
-		sdComm.write_Int(rwBuffer[j]);
-		sdComm.write_String("\r\n");
-	}
+	f_open(&fil, "logfile.txt", FA_WRITE | FA_OPEN_ALWAYS);
+	f_close(&fil);
 
 	for(;;)
 	{
